d9f1318db88f2bf1cf193d031778e28587e06682
wisp <wispwisp@users.noreply.github.com>
2022-09-14 14:56:41 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    context: path.join(__dirname, 'app'),
+    entry: ['./js/app.ts'],
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'app/index.html'),
+            inject: 'body'
+        })
+    ]
+};
