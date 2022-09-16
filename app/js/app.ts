379ae0fe207bf64aab58d0a82ddb0405df45d04c
wisp <forworkandtravel@yandex.ru>
2022-09-16 08:10:06 +0000

    initial code

--- app/js/app.ts
+++ app/js/app.ts
@@ -1,3 +1,30 @@
-// export default function sum(a: number, b: number): number {
-//     return a + b;
-// }
+import { draw, createParticlesGroup, rule } from './helpers';
+
+const a = <HTMLCanvasElement>document.getElementById('life');
+const c = <CanvasRenderingContext2D>a.getContext('2d');
+
+const width = a.offsetWidth;
+const height = a.offsetHeight;
+
+const particles: any[] = [];
+const yellow = createParticlesGroup(7, 'yellow', particles, width, height);
+const blue = createParticlesGroup(7, 'blue', particles, width, height);
+const red = createParticlesGroup(7, 'red', particles, width, height);
+
+const update = () => {
+    rule(yellow, yellow, -0.5);
+    rule(blue, blue, -0.1);
+
+    // Bounding
+    rule(blue, red, -1);
+    rule(yellow, red, -1);
+
+    c.clearRect(0, 0, width, height);
+    draw(c, 0, 0, 'black', width, height);
+    for (const particle of particles) {
+        draw(c, particle.x, particle.y, particle.color, 5, 5);
+    }
+    requestAnimationFrame(update);
+};
+
+update();
---
+++ app/js/helpers.ts
@@ -0,0 +1,80 @@
+export default {};
+
+export function sum(a: number, b: number): number {
+    return a + b;
+}
+
+export const draw = (
+    m: CanvasRenderingContext2D,
+    x: number,
+    y: number,
+    c: string,
+    w: number,
+    h: number
+) => {
+    m.fillStyle = c;
+    m.fillRect(x, y, w, h);
+};
+
+export const particle = (x: number, y: number, c: string) => {
+    return { x: x, y: y, vx: 0, vy: 0, color: c };
+};
+
+export const createParticlesGroup = (
+    amount: number,
+    color: string,
+    particles: any[],
+    width: number,
+    height: number
+): any[] => {
+    const group = [];
+    for (let i = 0; i < amount; i++) {
+        group.push(particle(Math.random() * width, Math.random() * height, color));
+        particles.push(group[i]);
+    }
+    return group;
+};
+
+export const rule = (particlesGroup1: any[], particlesGroup2: any[], g: number) => {
+    // assume mass of particle equal to 1
+
+    for (let i = 0; i < particlesGroup1.length; i++) {
+        let fx = 0;
+        let fy = 0;
+        let a: any;
+        let b: any;
+        for (let j = 0; j < particlesGroup1.length; j++) {
+            a = particlesGroup1[i];
+            b = particlesGroup2[j];
+
+            // distance
+            let dx = a.x - b.x;
+            let dy = a.y - b.y;
+            let d = Math.sqrt(dx * dx + dy * dy);
+
+            // Force
+            // Gravity force = G * (m1*m2 / distance)
+            // m1, m2 == 1, so:
+            // F = G*(1/distance)
+            if (d > 0) {
+                // assume mass equal to 1
+                let F = g * (1 / d);
+                fx += F * dx;
+                fy += F * dy;
+            }
+        }
+
+        // Update velocity by force
+        // ---
+        // F = ma
+        // a = F/m
+        // a = F/1
+        // a = F
+        a.vx = a.vx + fx;
+        a.vy = a.vy + fy;
+
+        // Update aplied force
+        a.x += fx;
+        a.y += fy;
+    }
+};
